bd24225ebf3f2bcf9df19a6045b645fb1496ecf6
Dmitrii Chikovinskii <github@dmitrii.space>
2023-01-12 15:49:46 +0700

    Added apns configuration

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,5 +1,6 @@
 import Vapor
 import APNS
+import JWTKit
 import QueuesMongoDriver
 
 public func configure(_ app: Application) async throws {
@@ -29,7 +30,20 @@ public func configure(_ app: Application) async throws {
     app.deviceServiceFactory.use(DeviceService.init)
     app.tonAccountServiceFactory.use(TonAccountService.init)
 
-
+    let pathToAPNSKey = Environment.get("TT_APNS_KEY_PATH")!
+    let apnsKeyIdentifier = Environment.get("TT_APNS_KEY_IDENTIFIER")!
+    let apnsTeamIdentifier = Environment.get("TT_APNS_TEAM_IDENTIFIER")!
+    let apnsTopic = Environment.get("TT_APNS_TOPIC")!
+    
+    app.apns.configuration = try .init(
+        authenticationMethod: .jwt(
+            key: .private(filePath: pathToAPNSKey),
+            keyIdentifier: JWKIdentifier(string: apnsKeyIdentifier),
+            teamIdentifier: apnsTeamIdentifier
+        ),
+        topic: apnsTopic,
+        environment: app.environment == .production ? .production : .sandbox
+    )
 
     // register routes
     try routes(app)
